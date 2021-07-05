b43480fedd24bce3d9165b7b916d2cf5047b2350
wani118 <wanidhanashri98@gmail.com>
2021-07-05 17:06:07 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: `
+  <h1>Startig angular again lol</h1>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'try-services';
+}
